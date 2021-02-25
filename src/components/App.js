89cfbe7070e21d4c4e3f576d53af3f4d48338ec5
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-02-25 11:58:34 -0600

    created response.data.items in set state

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,10 @@ import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 
 
-
-
 class App extends React.Component {
+    state = {videos: [] };
+
+
     onTermSubmit = async term => {
         console.log(term)
         const response = await youtube.get('/search', {
@@ -13,13 +14,15 @@ class App extends React.Component {
                 q: term
             }
         });
-        console.log(response);
+
+        this.setState({videos: response.data.items});
     };
 
     render() {
-        return(
+        return (
             <div className={'ui container'}>
                 <SearchBar callParentSubmit={this.onTermSubmit}/>
+                I have {this.state.videos.length} videos
             </div>
         )
 
