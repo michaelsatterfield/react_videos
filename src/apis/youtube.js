dcdc7977fdb4f78cd0a30cd7b87ae1c8892bc1f1
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-02-26 20:55:13 -0600

    cleaning code

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,23 +1,20 @@
 import axios from "axios";
-<<<<<<< HEAD
-import KEY from "../Keys";
-=======
-// import KEY from "../Keys";
 
-const KEY = 'AIzaSyDTEkFA7mK79sb_EAdQais6Gqhp7iHBMho';
->>>>>>> 9f8e53825c066bf08893ae8b1fd3d1c80381cc6e
+import KEY from "../Keys";
+
+
+
+
+
 
 export default axios.create({
     baseURL : 'https://www.googleapis.com/youtube/v3',
     params: {
         part: 'snippet',
         type: 'video',
-<<<<<<< HEAD
         maxResults: '10',
         key: 'KEY'
-=======
-        maxResults: '5',
-        key: KEY
->>>>>>> 9f8e53825c066bf08893ae8b1fd3d1c80381cc6e
+
+
     }
 });
